a8af803d2c6ce4e6a0d16d237660a5469a75dedc
Michael Newcomer <mcnewcomer@hotmail.com>
2018-06-21 23:35:25 -0700

    added friends

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    var newFriends = req.body;
+      console.log(newFriends);
+      friends.push(newFriends);
+      res.json(newFriends);
+
+  });
+
+};
